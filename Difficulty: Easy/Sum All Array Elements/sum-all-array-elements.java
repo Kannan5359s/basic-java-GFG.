b72d7d4a8275271d9d19af222d48fe99c86deafd
Kannan.M <mkannan0207@gmail.com>
2025-09-12 21:03:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int arraySum(int[] arr) {
+        // code here
+        int a=0;
+        for (int i:arr){
+            a+=i;
+        }
+        return a;
+    }
+}
