f22c7bdef89cc1d57b04882b3e5e63116a38a584
Kannan.M <mkannan0207@gmail.com>
2025-09-20 07:36:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of String/length-of-string.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    // Complete the function
+    // str: input string
+    public static int lengthString(String str) {
+        return (str.length());
+    }
+}
