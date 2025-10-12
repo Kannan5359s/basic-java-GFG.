108ac004c5d2d389721e34c674a4c9ef3799b560
Kannan.M <mkannan0207@gmail.com>
2025-10-12 20:26:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of primes/sum-of-primes.java
@@ -0,0 +1,17 @@
+// User function Template for Java
+
+class Solution {
+    static int primeSum(int n) {
+        // code here
+        int sum=0;
+        while(n>0){
+            int digit=n%10;
+            
+            if(digit == 2 || digit == 3 || digit == 5 || digit == 7)
+                sum += digit;
+
+            n/=10;
+        }
+        return sum;
+    }
+}
